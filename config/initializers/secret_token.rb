9dcd99d81d6a81c5d2a7941eea6842e38e4d2617
Jorge Banguera <jorge.banguera@YUXIDEV-JBANGUERA.local>
2016-05-31 15:18:47 -0500

    First commit, scaffolded user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Apexblog::Application.config.secret_key_base = 'cd722cde2ae939d4d10a3b11cb5745fa1546e91ad1011dce66434297e4243d67ac79138b3d1e18c5cb54280fd3f4a72f1ec7be40bb80e0888d00dd761081e7bf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Apexblog::Application.config.session_store :cookie_store, key: '_apexblog_session'
