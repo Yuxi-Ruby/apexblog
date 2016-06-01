b9df4583fe93848896d60b9dd10cf9bc4ea6625b
“Hernan <hernan.sanchez@yuxipacific.com>
2016-06-01 14:10:02 -0500

    Add test User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
+	test "exist user model" do
+		User_model_exist = users
+		assert_not_nil User_model_exist, "User model not exists"
+	end
+
+	test "exist user two" do
+		User_two_exist = users(:two)
+		assert User_two_exist, "Two user not exist"
+	end
+
+	test "name_field_presence" do
+		user_name = users(:one).name
+		assert_not_nil user_name, "Name field not presence"
+	end
+
+	test "email_field_presence" do
+		user_email = users(:one).email
+		assert_not_nil user_email, "Email field not presence"
+	end
+
+	test "email_field_format_valid" do
+		user_email = users(:one).email
+		assert_match /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/, user_email, "Email field not format_valid"
+	end
+
+	test "email_unique" do
+		user_new = User.new(:name => 'Andrea', :email => 'stevssen@hotmail.com')
+		assert user_new.valid?, "¡Email field exist!"
+	end
 end
