7c7df2f4fb2eb20d599498b562d91cf590cff4be
Ricardo Rojas <ricardo.rojas@Pablos-iMac.local>
2016-06-01 14:23:56 -0500

    destroy scaffold user

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ActiveRecord::Base
-  validates :name, presence: true, length: {minimum: 1, maximum: 10}
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
-end
