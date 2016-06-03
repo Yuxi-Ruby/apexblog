f0ce52583aa08747527185392bea0c5314f65442
jamessantoyamendez <james.santoya@yuxipacific.com>
2016-06-03 16:51:45 -0500

    Front-end home page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
 	validates :title, presence: true,uniqueness: true, length:{ maximum: 20, minimum: 7 }
-	def convert_capitalize(string) 	
-	 	string.to_s.capitalize  
+	def convert_capitalize(string)
+	 	string.to_s.capitalize
 	end
 end
