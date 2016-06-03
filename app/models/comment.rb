b090490eacf31e9b9fc5dd631bae448e3a3a86e3
erestrepo <esteban.restrepo@yuxipacific.com>
2016-06-03 11:24:45 -0500

    Remove incorrect information for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,6 @@ class Comment < ActiveRecord::Base
    validates :description, presence: true, length: { minimum: 2 }
    validates :user, presence: true
    validates :article, presence: true
-   # validates_associated :article
-   # validates_associated :user
 
    belongs_to :article
    belongs_to :user
--- db/migrate/20160531230143_create_comments.rb
+++ db/migrate/20160531230143_create_comments.rb
@@ -2,9 +2,6 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :description
-      t.integer :author
-      t.integer :article
-
       t.timestamps
     end
   end
