730d83e0a3420e03472766ac1c7243ed6ad01f0e
Andres Diaz <andres.diaz@yuxipacific.com>
2016-07-13 15:46:04 -0500

    Improve UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,11 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+    @categories = Category.all
   end
 
   def show
-  	@article = Article.find(params[:id])
+    @article = Article.find(params[:id])
+    @categories = Category.all
   end
 end
