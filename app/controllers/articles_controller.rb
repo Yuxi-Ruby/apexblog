aa10c5ad95e84ee938f57004577882e4dd949ed5
Fabian Buitrago <fabianbuitragov@gmail.com>
2018-02-05 08:29:00 -0500

    Added keyword search to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,20 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = if params[:term]
+      Article.search_anywhere(params[:term])
+    else
+      Article.all
+    end
+    
     @categories = Category.all
   end
 
   def show
     @article = Article.find(params[:id])
     @categories = Category.all
+  end
+
+  def article_params
+    params.require(:task).permit(:title, :complete, :due_date, :term)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -22,4 +22,6 @@ class Article < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_and_belongs_to_many :categories
+
+  search_fields [:title, :description, :body]
 end
