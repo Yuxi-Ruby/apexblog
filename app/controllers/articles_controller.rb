32633322d4455bc7fe66dbec0eb3d27581403966
Fabian Buitrago <fabianbuitragov@gmail.com>
2018-05-24 17:19:20 -0500

    Added next and back buttons for navigation of articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,9 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @articles = Article.order('id ASC')
+    current_page = @articles.index{|article|article.id == params[:id].to_i}
+    @navigation = {:current_page => current_page, :total_pages => @articles.count }
     @categories = Category.all
   end
 end
