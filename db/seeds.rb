0675d6dd8bae2b0e65d6e087517bc20981dcf810
Fabian Buitrago <fabianbuitragov@gmail.com>
2018-06-01 16:19:29 -0500

    added initial config to create test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!([
+  { name: 'Demo User', email:'demo@yuxiglobal.com', password: "demo1234", password_confirmation: "demo1234" }
+])
+
+Article.create!([
+  { title: 'Demo title article', description:'Demo description article', body: "Demo body article", user_id: User.find_by_name('Demo User').id }
+])
+
+Comment.create!([
+  { description: 'Demo description comment', article_id: Article.find_by_title('Demo title article').id, user_id: User.find_by_name('Demo User').id }
+])
+
+Category.create!([
+  { title: 'Rails'},
+  { title: 'Angular'}
+])
